c0eed8e50f63dde38ab0a112fba8e047502ca196
Alex Burlacu <alexburlacu96@gmail.com>
2018-05-03 11:37:06 +0300

    define the library structure

---
+++ lab5/lib/custom_tcp_protocol/client/client.ex
@@ -0,0 +1,27 @@
+defmodule CustomTCPProtocol.Client do
+    @moduledoc """
+    Client side of the CustomTCPProtocol library
+
+    :func: connect
+    :func: exit
+    :func: help
+    :func: exec
+    """
+
+    def connect do
+        
+    end
+
+    def help do
+        
+    end
+
+    def exec do
+        
+    end
+
+    def exit do
+        
+    end
+    
+end
---
+++ lab5/lib/custom_tcp_protocol/server/server.ex
@@ -0,0 +1,12 @@
+defmodule CustomTCPProtocol.Server do
+    @moduledoc """
+    Sever side of the CustomTCPProtocol library
+
+    :func: start
+    """
+
+    def start(address, port) do
+        
+    end
+
+end
