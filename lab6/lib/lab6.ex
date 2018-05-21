84f28a5df3698080a818044d8404c46b0fd95968
Alex Burlacu <alexburlacu96@gmail.com>
2018-05-21 18:40:07 +0300

    create user and send message functionality added

--- lab6/lib/lab6.ex
+++ lab6/lib/lab6.ex
@@ -3,33 +3,37 @@ defmodule Lab6 do
     Documentation for Lab6.
     """
 
-    def send_msg(user_msg, to, from) do
-        msg = :base64.encode "{:type :chat, :txt \"#{msg}\"}"
+    defp send_udp(data) do
+        {:ok, sock} = :gen_udp.open 61616, [:binary]
+        :ok = :gen_udp.send(sock, '230.185.192.108', 42424, data)
+        :gen_udp.close(sock)
+    end
+
+    def send_msg(user_msg, from_uuid, to_uuid) do
+        msg = :base64.encode "{:type :chat, :txt \"#{user_msg}\"}"
         timestamp = :os.system_time(:milli_seconds)
-        from_uuid = from
-        to_uuid = to
         data = "#{timestamp}|#{from_uuid}|#{to_uuid}|#{msg}"
                |> :base64.encode
                |> String.to_charlist
                |> :erlang.term_to_binary
 
-        {:ok, sock} = :gen_udp.open 61616, [:binary]
-        :ok = :gen_udp.send(conn, 'localhost', 42424, data)
-        :gen_udp.close(sock)
+        send_udp data
     end
 
     def create_user(name) do
         msg = :base64.encode "{:type :online, :username \"#{name}\"}"
         timestamp = :os.system_time(:milli_seconds)
-        uuid = 
+        uuid = UUID.uuid4()
 
         data = "#{timestamp}|#{uuid}|:all|#{msg}"
                |> :base64.encode
                |> String.to_charlist
                |> :erlang.term_to_binary
 
-        {:ok, sock} = :gen_udp.open 61615, [:binary]
-        :ok = :gen_udp.send(conn, 'localhost', 42424, data)
-        :gen_udp.close(sock)
+        IO.inspect data
+
+        send_udp data
+
+        uuid
     end
 end
