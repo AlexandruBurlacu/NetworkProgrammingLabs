289720c6bcf2ce4bcdb22b5f57ec03a2c1200891
Alex Burlacu <alexburlacu96@gmail.com>
2018-05-30 09:58:01 +0300

    done

--- lab4/lib/login.ex
+++ lab4/lib/login.ex
@@ -1,8 +1,14 @@
 defmodule Lab4.Login do
 
-  def login (username, password) do
+  def login(username, password) do
     Application.put_env :mailbox, "SMTP_USERNAME", username
     Application.put_env :mailbox, "SMTP_PASSWORD", password
   end
 
+  def logout() do
+    Application.put_env :mailbox, "SMTP_USERNAME", ""
+    Application.put_env :mailbox, "SMTP_PASSWORD", ""
+  end
+
 end
+
